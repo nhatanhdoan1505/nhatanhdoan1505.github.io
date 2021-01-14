7b8c0888f4a62ce3c1bad5945bef10bb431178b0
Doan Nhat Anh <nhatanhdoan1505@gmail.com>
2021-01-14 10:12:25 +0700

    Commit

---
+++ precache-manifest.43e5dc637a9d61dfcd1f9ff72e81fb3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7036ced2d661d20fa553030cac2c5fa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "833358eddbb90e1b018e",
+    "url": "/static/css/main.b54eeb13.chunk.css"
+  },
+  {
+    "revision": "43b0ad7d32787a84ea1c",
+    "url": "/static/js/2.e4c23c69.chunk.js"
+  },
+  {
+    "revision": "cc9a9584762785f97aa7065f1b7a200a",
+    "url": "/static/js/2.e4c23c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833358eddbb90e1b018e",
+    "url": "/static/js/main.27ab7969.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  },
+  {
+    "revision": "e57071b5ad62f86f4ef617730d281fb3",
+    "url": "/static/media/background.e57071b5.jpg"
+  },
+  {
+    "revision": "b164c8c067dc0bdfe083b2e1452d4db9",
+    "url": "/static/media/image.b164c8c0.jpg"
+  }
+]);
---
+++ precache-manifest.5fd58c74107bcf494561dbd64d6849e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d822bf051dcb003eec85d295e4da428d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f306e96d9bb6ce499ae2",
+    "url": "/static/css/main.8a7e7b51.chunk.css"
+  },
+  {
+    "revision": "43f927420dcd2216375a",
+    "url": "/static/js/2.d144f112.chunk.js"
+  },
+  {
+    "revision": "6177d1ab9ae3c0c435b4d0c754a1b33b",
+    "url": "/static/js/2.d144f112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f306e96d9bb6ce499ae2",
+    "url": "/static/js/main.cd0d1fc1.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  },
+  {
+    "revision": "e57071b5ad62f86f4ef617730d281fb3",
+    "url": "/static/media/background.e57071b5.jpg"
+  },
+  {
+    "revision": "b164c8c067dc0bdfe083b2e1452d4db9",
+    "url": "/static/media/image.b164c8c0.jpg"
+  }
+]);
